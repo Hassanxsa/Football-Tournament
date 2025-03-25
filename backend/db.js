e67939aa07a3eefb98a4c20e3502465c4f48e498
Hassan <51971397+Hassanxsa@users.noreply.github.com>
2025-03-25 17:25:52 +0300

    Add database server

---
+++ backend/db.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const {Client} = require("pg");
+
+const client = new Client({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false,
+      }
+});
+
+client.connect().then(() =>
+     console.log("Connected to PostegreSQL"))
+     .catch(err => console.error("Connection error:", err));;
